cb6e1db9fbbd08bb66651ac19e8bbe94bbc195e7
psu6810110570 <chindanai.ac@gmail.com>
2026-03-06 15:19:47 +0700

    ประกอบระบบ Threading และเพิ่มตัวจับเวลา

--- src/01_use_thread.py
+++ src/01_use_thread.py
@@ -10,3 +10,35 @@ CITIES = [
     {"name": "New York", "lat": 40.71, "lon": -74.00},
     {"name": "Sydney", "lat": -33.86, "lon": 151.20}
 ]
+
+def fetch_weather(city):
+    # ฟังก์ชันดึงข้อมูลสภาพอากาศ 1 เมือง
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={city['lat']}&longitude={city['lon']}&current_weather=true"
+    
+    response = requests.get(url)
+    data = response.json()
+    
+    temp = data["current_weather"]["temperature"]
+    print(f"[Thread] {city['name']} อุณหภูมิ: {temp}°C")
+
+def main():
+    print("เริ่มดึงข้อมูลด้วย Threading...")
+    start_time = time.time()
+    
+    threads = []
+    
+    # Thread สำหรับแต่ละเมือง
+    for city in CITIES:
+        t = threading.Thread(target=fetch_weather, args=(city,))
+        threads.append(t)
+        t.start()
+        
+    #รอให้ทุก Thread ทำงานเสร็จ
+    for t in threads:
+        t.join()
+        
+    end_time = time.time()
+    print(f"ใช้เวลาทั้งหมด: {end_time - start_time:.4f} วินาที")
+
+if __name__ == "__main__":
+    main()
