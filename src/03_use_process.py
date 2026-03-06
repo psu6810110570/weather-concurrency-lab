e2dbef461e25116317cc1318fd84f835f26455ea
psu6810110570 <chindanai.ac@gmail.com>
2026-03-06 15:35:25 +0700

    feat : ฟังก์ชัน fetch_weather_process

--- src/03_use_process.py
+++ src/03_use_process.py
@@ -9,3 +9,13 @@ CITIES = [
     {"name": "New York", "lat": 40.71, "lon": -74.00},
     {"name": "Sydney", "lat": -33.86, "lon": 151.20}
 ]
+
+def fetch_weather_process(city):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={city['lat']}&longitude={city['lon']}&current_weather=true&timezone=auto"
+    start_req = time.time()
+    response = requests.get(url)
+    data = response.json()
+    fetch_time = time.time() - start_req
+    temp = data["current_weather"]["temperature"]
+    local_time = data["current_weather"]["time"].replace("T", " ")
+    print(f"[Process] {city['name']:<10} อุณหภูมิ: {temp}°C (เวลา: {local_time}) | ดึงเสร็จใน: {fetch_time:.4f} วินาที")
